fb3367ac995d918015efff46ab1c0760f8179c60
azolic <azolic@campus.tu-berlin.de>
2018-02-15 09:18:56 +0100

    Added separate threads and writing output to csv file.

--- src/main/java/main/PerformanceMonitor.java
+++ src/main/java/main/PerformanceMonitor.java
@@ -9,17 +9,23 @@ import oshi.hardware.HWPartition;
 import oshi.util.FormatUtil;
 import oshi.util.Util;
 
+import java.io.*;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.Timer;
 import java.util.TimerTask;
 
-public class PerformanceMonitor { //extends TimerTask
+public class PerformanceMonitor implements Runnable {
 
     SystemInfo systemInfo;
     CentralProcessor cpu;
     GlobalMemory memory;
     HWDiskStore[] disks;
 
+    String output = "output.csv";
+    PrintWriter writer;
+
+    boolean run = true;
 
     public PerformanceMonitor() {
         systemInfo = new SystemInfo();
@@ -27,12 +33,32 @@ public class PerformanceMonitor { //extends TimerTask
         cpu = systemInfo.getHardware().getProcessor();
         memory = systemInfo.getHardware().getMemory();
         disks = systemInfo.getHardware().getDiskStores();
+
+        try {
+            writer = new PrintWriter(new File(output));
+            writer.write("CPU load(ticks), CPU load(OS MX Bean),,CPU load per processor\n");
+        } catch (FileNotFoundException e) {
+            System.out.println("Cant open file");
+            e.printStackTrace();
+        }
+
     }
 
-//    @Override
-//    public void run() {
-//        this.printStats();
-//    }
+    @Override
+    public void run() {
+
+        while (run) {
+            this.printStats();
+            //System.out.println("running");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+            }
+        }
+        System.out.println("Done running");
+
+        writer.close();
+    }
 
     public void printGeneralInfo() {
         System.out.println("Processor: " + cpu.getName());
@@ -62,42 +88,52 @@ public class PerformanceMonitor { //extends TimerTask
         }
     }
 
-    private static void printCpu(CentralProcessor processor) {
-        System.out.println("Uptime: " + FormatUtil.formatElapsedSecs(processor.getSystemUptime()));
+    private void printCpu(CentralProcessor processor) {
+        StringBuilder result = new StringBuilder();
+        //System.out.println("Uptime: " + FormatUtil.formatElapsedSecs(processor.getSystemUptime()));
 
-        long[] prevTicks = processor.getSystemCpuLoadTicks();
-        System.out.println("CPU, IOWait, and IRQ ticks @ 0 sec:" + Arrays.toString(prevTicks));
+        //long[] prevTicks = processor.getSystemCpuLoadTicks();
+        //System.out.println("CPU, IOWait, and IRQ ticks @ 0 sec:" + Arrays.toString(prevTicks));
         // Wait a second...
-        Util.sleep(1000);
-        long[] ticks = processor.getSystemCpuLoadTicks();
-        System.out.println("CPU, IOWait, and IRQ ticks @ 1 sec:" + Arrays.toString(ticks));
-        long user = ticks[TickType.USER.getIndex()] - prevTicks[TickType.USER.getIndex()];
-        long nice = ticks[TickType.NICE.getIndex()] - prevTicks[TickType.NICE.getIndex()];
-        long sys = ticks[TickType.SYSTEM.getIndex()] - prevTicks[TickType.SYSTEM.getIndex()];
-        long idle = ticks[TickType.IDLE.getIndex()] - prevTicks[TickType.IDLE.getIndex()];
-        long iowait = ticks[TickType.IOWAIT.getIndex()] - prevTicks[TickType.IOWAIT.getIndex()];
-        long irq = ticks[TickType.IRQ.getIndex()] - prevTicks[TickType.IRQ.getIndex()];
-        long softirq = ticks[TickType.SOFTIRQ.getIndex()] - prevTicks[TickType.SOFTIRQ.getIndex()];
-        long steal = ticks[TickType.STEAL.getIndex()] - prevTicks[TickType.STEAL.getIndex()];
-        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
+        //Util.sleep(1000);
+        //long[] ticks = processor.getSystemCpuLoadTicks();
+        //System.out.println("CPU, IOWait, and IRQ ticks @ 1 sec:" + Arrays.toString(ticks));
+//        long user = ticks[TickType.USER.getIndex()] - prevTicks[TickType.USER.getIndex()];
+//        long nice = ticks[TickType.NICE.getIndex()] - prevTicks[TickType.NICE.getIndex()];
+//        long sys = ticks[TickType.SYSTEM.getIndex()] - prevTicks[TickType.SYSTEM.getIndex()];
+//        long idle = ticks[TickType.IDLE.getIndex()] - prevTicks[TickType.IDLE.getIndex()];
+//        long iowait = ticks[TickType.IOWAIT.getIndex()] - prevTicks[TickType.IOWAIT.getIndex()];
+//        long irq = ticks[TickType.IRQ.getIndex()] - prevTicks[TickType.IRQ.getIndex()];
+//        long softirq = ticks[TickType.SOFTIRQ.getIndex()] - prevTicks[TickType.SOFTIRQ.getIndex()];
+//        long steal = ticks[TickType.STEAL.getIndex()] - prevTicks[TickType.STEAL.getIndex()];
+//        long totalCpu = user + nice + sys + idle + iowait + irq + softirq + steal;
 
-        System.out.format(
-                "User: %.1f%% Nice: %.1f%% System: %.1f%% Idle: %.1f%% IOwait: %.1f%% IRQ: %.1f%% SoftIRQ: %.1f%% Steal: %.1f%%%n",
-                100d * user / totalCpu, 100d * nice / totalCpu, 100d * sys / totalCpu, 100d * idle / totalCpu,
-                100d * iowait / totalCpu, 100d * irq / totalCpu, 100d * softirq / totalCpu, 100d * steal / totalCpu);
-        System.out.format("CPU load: %.1f%% (counting ticks)%n", processor.getSystemCpuLoadBetweenTicks() * 100);
-        System.out.format("CPU load: %.1f%% (OS MXBean)%n", processor.getSystemCpuLoad() * 100);
-        double[] loadAverage = processor.getSystemLoadAverage(3);
-        System.out.println("CPU load averages:" + (loadAverage[0] < 0 ? " N/A" : String.format(" %.2f", loadAverage[0]))
-                + (loadAverage[1] < 0 ? " N/A" : String.format(" %.2f", loadAverage[1]))
-                + (loadAverage[2] < 0 ? " N/A" : String.format(" %.2f", loadAverage[2])));
+//        System.out.format(
+//                "User: %.1f%% Nice: %.1f%% System: %.1f%% Idle: %.1f%% IOwait: %.1f%% IRQ: %.1f%% SoftIRQ: %.1f%% Steal: %.1f%%%n",
+//                100d * user / totalCpu, 100d * nice / totalCpu, 100d * sys / totalCpu, 100d * idle / totalCpu,
+//                100d * iowait / totalCpu, 100d * irq / totalCpu, 100d * softirq / totalCpu, 100d * steal / totalCpu);
+
+        //System.out.format("CPU load: %.1f%% (counting ticks)%n", processor.getSystemCpuLoadBetweenTicks() * 100);
+        result.append(String.format("%.1f%%,", processor.getSystemCpuLoadBetweenTicks() * 100)); // CPU load (counting ticks)
+        //System.out.format("CPU load: %.1f%% (OS MXBean)%n", processor.getSystemCpuLoad() * 100);
+        result.append(String.format("%.1f%%,,", processor.getSystemCpuLoad() * 100)); // CPU load (OS MXBean)
+        // AVG ONLY GIVES N/A
+        //        double[] loadAverage = processor.getSystemLoadAverage(3);
+//        System.out.println("CPU load averages:" + (loadAverage[0] < 0 ? " N/A" : String.format(" %.2f", loadAverage[0]))
+//                + (loadAverage[1] < 0 ? " N/A" : String.format(" %.2f", loadAverage[1]))
+//                + (loadAverage[2] < 0 ? " N/A" : String.format(" %.2f", loadAverage[2])));
         // per core CPU
-        StringBuilder procCpu = new StringBuilder("CPU load per processor:");
+
+        StringBuilder procCpu = new StringBuilder();//"CPU load per processor:"
         double[] load = processor.getProcessorCpuLoadBetweenTicks();
         for (double avg : load) {
-            procCpu.append(String.format(" %.1f%%", avg * 100));
+            procCpu.append(String.format("%.1f%%,", avg * 100));
         }
-        System.out.println(procCpu.toString());
+        //System.out.println(procCpu.toString());
+        result.append(procCpu);
+        result.append("\n");
+
+        writer.write(result.toString());
     }
 
 
@@ -117,7 +153,7 @@ public class PerformanceMonitor { //extends TimerTask
                 continue;
             }
             for (HWPartition part : partitions) {
-                System.out.format(" |-- %s: %s (%s) Maj:Min=%d:%d, size: %s%n", part.getIdentification(),
+                System.out.format(" |-- %s: %s (%s) Maj:Min=%d:%d, size: %s/n", part.getIdentification(),
                         part.getName(), part.getType(), part.getMajor(), part.getMinor(),
                         FormatUtil.formatBytesDecimal(part.getSize()),
                         (part.getMountPoint().isEmpty()) ? "" : (" @ " + part.getMountPoint()));
@@ -127,25 +163,37 @@ public class PerformanceMonitor { //extends TimerTask
     }
 
     public void printStats() {
-        System.out.println("Processor info:");
+        //System.out.println("Processor info:");
 
         printCpu(cpu);
 
         //System.out.println("Memory info:");
 
-        System.out.println("Disk info:");
-        printDisks(disks);
+        //System.out.println("Disk info:");
+        //printDisks(disks);
+    }
+
+    public void setRun(boolean run) {
+        this.run = run;
     }
 
 
     public static void main(String[] args) {
         PerformanceMonitor monitor = new PerformanceMonitor();
+        Scanner s = new Scanner(System.in);
+        Thread t = new Thread(monitor);
 
-        //monitor.printGeneralInfo();
-        //monitor.printStats();
 
-        //Timer timer = new Timer();
-       // timer.schedule(monitor, 0, 5000);
+        System.out.println("Enter 's' to start, 'q' to quit:");
+        if (s.next().toLowerCase().equals("s")) {
+            System.out.println("=========started=========");
+            t.start();
+        }
 
+        while (!s.next().toLowerCase().equals("q")) ;
+
+        monitor.run = false;
+        t.interrupt();
+        System.out.println("=========ended=========");
     }
 }
