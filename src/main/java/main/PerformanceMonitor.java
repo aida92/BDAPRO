9cf3c0deff342cdad74ef82001dab21010a69a1c
azolic <azolic@campus.tu-berlin.de>
2018-02-15 11:32:46 +0100

    CPU and I/O
    Added exapmple of csv output

--- src/main/java/main/PerformanceMonitor.java
+++ src/main/java/main/PerformanceMonitor.java
@@ -36,9 +36,9 @@ public class PerformanceMonitor implements Runnable {
 
         try {
             writer = new PrintWriter(new File(output));
-            writer.write("CPU load(ticks), CPU load(OS MX Bean),,CPU load per processor\n");
+            writer.write("CPU % load(ticks), CPU % load(OS MX Bean),Reads,Reads(GiB),Writes,Writes(GiB)\n");
         } catch (FileNotFoundException e) {
-            System.out.println("Cant open file");
+            System.out.println("Cannot open the file!");
             e.printStackTrace();
         }
 
@@ -46,17 +46,13 @@ public class PerformanceMonitor implements Runnable {
 
     @Override
     public void run() {
-
         while (run) {
             this.printStats();
-            //System.out.println("running");
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException e) {
             }
         }
-        System.out.println("Done running");
-
         writer.close();
     }
 
@@ -114,9 +110,9 @@ public class PerformanceMonitor implements Runnable {
 //                100d * iowait / totalCpu, 100d * irq / totalCpu, 100d * softirq / totalCpu, 100d * steal / totalCpu);
 
         //System.out.format("CPU load: %.1f%% (counting ticks)%n", processor.getSystemCpuLoadBetweenTicks() * 100);
-        result.append(String.format("%.1f%%,", processor.getSystemCpuLoadBetweenTicks() * 100)); // CPU load (counting ticks)
+        result.append(String.format("%.1f,", processor.getSystemCpuLoadBetweenTicks() * 100)); // CPU load (counting ticks)
         //System.out.format("CPU load: %.1f%% (OS MXBean)%n", processor.getSystemCpuLoad() * 100);
-        result.append(String.format("%.1f%%,,", processor.getSystemCpuLoad() * 100)); // CPU load (OS MXBean)
+        result.append(String.format("%.1f,", processor.getSystemCpuLoad() * 100)); // CPU load (OS MXBean)
         // AVG ONLY GIVES N/A
         //        double[] loadAverage = processor.getSystemLoadAverage(3);
 //        System.out.println("CPU load averages:" + (loadAverage[0] < 0 ? " N/A" : String.format(" %.2f", loadAverage[0]))
@@ -124,41 +120,54 @@ public class PerformanceMonitor implements Runnable {
 //                + (loadAverage[2] < 0 ? " N/A" : String.format(" %.2f", loadAverage[2])));
         // per core CPU
 
-        StringBuilder procCpu = new StringBuilder();//"CPU load per processor:"
-        double[] load = processor.getProcessorCpuLoadBetweenTicks();
-        for (double avg : load) {
-            procCpu.append(String.format("%.1f%%,", avg * 100));
-        }
+//        StringBuilder procCpu = new StringBuilder();//"CPU load per processor:"
+//        double[] load = processor.getProcessorCpuLoadBetweenTicks();
+//        for (double avg : load) {
+//            procCpu.append(String.format("%.1f%%,", avg * 100));
+//        }
         //System.out.println(procCpu.toString());
-        result.append(procCpu);
-        result.append("\n");
+        //result.append(procCpu);
+        //result.append("\n");
 
         writer.write(result.toString());
     }
 
 
-    private static void printDisks(HWDiskStore[] diskStores) {
-        System.out.println("Disks:");
+    public void printDisks(HWDiskStore[] diskStores) {
+        StringBuilder result = new StringBuilder();
+        //System.out.println("Disks:");
+        boolean first = true;
         for (HWDiskStore disk : diskStores) {
-            boolean readwrite = disk.getReads() > 0 || disk.getWrites() > 0;
-            System.out.format(" %s: (model: %s - S/N: %s) size: %s, reads: %s (%s), writes: %s (%s), xfer: %s ms%n",
-                    disk.getName(), disk.getModel(), disk.getSerial(),
-                    disk.getSize() > 0 ? FormatUtil.formatBytesDecimal(disk.getSize()) : "?",
-                    readwrite ? disk.getReads() : "?", readwrite ? FormatUtil.formatBytes(disk.getReadBytes()) : "?",
-                    readwrite ? disk.getWrites() : "?", readwrite ? FormatUtil.formatBytes(disk.getWriteBytes()) : "?",
-                    readwrite ? disk.getTransferTime() : "?");
-            HWPartition[] partitions = disk.getPartitions();
-            if (partitions == null) {
-                // TODO Remove when all OS's implemented
-                continue;
-            }
-            for (HWPartition part : partitions) {
-                System.out.format(" |-- %s: %s (%s) Maj:Min=%d:%d, size: %s/n", part.getIdentification(),
-                        part.getName(), part.getType(), part.getMajor(), part.getMinor(),
-                        FormatUtil.formatBytesDecimal(part.getSize()),
-                        (part.getMountPoint().isEmpty()) ? "" : (" @ " + part.getMountPoint()));
+            if  (!first)
+                result.append(",,");
 
-            }
+            first = false;
+
+            boolean readwrite = disk.getReads() > 0 || disk.getWrites() > 0;
+//            System.out.format(" %s: (model: %s - S/N: %s) size: %s, reads: %s (%s), writes: %s (%s), xfer: %s ms%n",
+//                    disk.getName(), disk.getModel(), disk.getSerial(),
+//                    disk.getSize() > 0 ? FormatUtil.formatBytesDecimal(disk.getSize()) : "?",
+//                    readwrite ? disk.getReads() : "?", readwrite ? FormatUtil.formatBytes(disk.getReadBytes()) : "?",
+//                    readwrite ? disk.getWrites() : "?", readwrite ? FormatUtil.formatBytes(disk.getWriteBytes()) : "?",
+//                    readwrite ? disk.getTransferTime() : "?");
+            result.append(readwrite ? disk.getReads() + "," : "?,");
+            result.append(readwrite ? FormatUtil.formatBytes(disk.getReadBytes()) + "," : "?,");
+            result.append(readwrite ? disk.getWrites() + "," : "?,");
+            result.append(readwrite ? FormatUtil.formatBytes(disk.getWriteBytes()) + "\n" : "?\n");
+
+            writer.write(result.toString());
+//            HWPartition[] partitions = disk.getPartitions();
+//            if (partitions == null) {
+//                // TODO Remove when all OS's implemented
+//                continue;
+//            }
+//            for (HWPartition part : partitions) {
+//                System.out.format(" |-- %s: %s (%s) Maj:Min=%d:%d, size: %s/n", part.getIdentification(),
+//                        part.getName(), part.getType(), part.getMajor(), part.getMinor(),
+//                        FormatUtil.formatBytesDecimal(part.getSize()),
+//                        (part.getMountPoint().isEmpty()) ? "" : (" @ " + part.getMountPoint()));
+
+            //}
         }
     }
 
@@ -170,7 +179,7 @@ public class PerformanceMonitor implements Runnable {
         //System.out.println("Memory info:");
 
         //System.out.println("Disk info:");
-        //printDisks(disks);
+        printDisks(disks);
     }
 
     public void setRun(boolean run) {
@@ -183,6 +192,7 @@ public class PerformanceMonitor implements Runnable {
         Scanner s = new Scanner(System.in);
         Thread t = new Thread(monitor);
 
+        //monitor.printDisks(monitor.disks);
 
         System.out.println("Enter 's' to start, 'q' to quit:");
         if (s.next().toLowerCase().equals("s")) {
@@ -194,6 +204,6 @@ public class PerformanceMonitor implements Runnable {
 
         monitor.run = false;
         t.interrupt();
-        System.out.println("=========ended=========");
+        System.out.println("==========ended==========");
     }
 }
